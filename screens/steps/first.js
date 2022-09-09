de0c26ca61ca1fe7dcaaba1c5f0f5167404ced60
Francisco Herrera <javierherrera52@gmail.com>
2022-09-09 01:10:37 -0700

    steps for form

--- screens/steps/first.js
+++ screens/steps/first.js
@@ -3,9 +3,9 @@ import {View, TextInput, Text} from 'react-native'
 
 const First = () => {
   return (
-    <View className='flex flex-col'>
-      <Text>First Step</Text>
-      <TextInput className='bg-app-50'/>
+    <View className='flex flex-col p-4'>
+      <Text className='text-5xl font-extrabold pb-8'>What's your student email?</Text>
+      <TextInput className='bg-app-50 w-full h-12 rounded-xl px-4 text-xl' placeholder="email"/>
     </View>
 
   )
--- screens/steps/second.js
+++ screens/steps/second.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import {View, TextInput, Text} from 'react-native'
+import {View, TextInput, Text, Pressable} from 'react-native'
 
-const Second = () => {
+const First = () => {
   return (
-    <View className='flex flex-col'>
-      <Text>Second Step</Text>
-      <TextInput className='bg-app-50'/>
+    <View className='flex flex-col p-4'>
+      <Text className='text-5xl font-extrabold pb-8'>Verify your student email</Text>
+      <Text className='text-lg'>Enter the code we've sent by email to</Text>
+      <View className='flex flex-row pb-4 items-center'>
+        <Text className='text-lg'>placeholder@email.com </Text>
+        <Pressable><Text className='text-lg underline font-bold'>Change</Text></Pressable>
+      </View>
+      <TextInput className='bg-app-50 w-full h-12 rounded-xl px-4 text-xl' placeholder="email"/>
     </View>
 
   )
 }
 
-export default Second
+export default First
